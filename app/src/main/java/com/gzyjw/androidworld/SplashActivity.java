7973bb1ae855ec4edf090c2eb20ced6cde7ec139
gzyjw <gzyjw@hotmail.com>
2018-06-21 14:32:57 +0800

    Initial commit

---
+++ app/src/main/java/com/gzyjw/androidworld/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.gzyjw.androidworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.jdom2.Document;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Document doc = new Document();
+
+    }
+}
